bb1397382a60f3c42365781f4b6ac2876e1152e7
Sophie de Jong <sophiemdejong@gmail.com>
2024-05-13 17:56:27 -0400

    changed name from skibc to ski

--- src/context.rs
+++ src/context.rs
@@ -4,7 +4,7 @@ use crate::lexer::*;
 use std::collections::HashMap;
 use std::path::PathBuf;
 
-pub const FILE_EXTENSION: &str = "skibc";
+pub const FILE_EXTENSION: &str = "ski";
 const MAX_RECURSION_DEPTH: usize = 1024;
 
 #[derive(Debug)]
